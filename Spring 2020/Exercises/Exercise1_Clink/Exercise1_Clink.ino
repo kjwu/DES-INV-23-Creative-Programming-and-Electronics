d629bdfd5cc3cbe6ecb985e97e6489068cc8f60c
kjwu <kjwu.co@gmail.com>
2020-01-29 13:50:23 -0800

    update clink exercise

--- Spring 2020/Exercises/Exercise1_Clink/Exercise1_Clink.ino
+++ Spring 2020/Exercises/Exercise1_Clink/Exercise1_Clink.ino
@@ -5,16 +5,31 @@
 */
 
 #define bpm 60 //Allegro:120bpm Moderato:90bpm Lento:60bpm
-#define solenoidPin 2
+#define solenoidPin 13
 
 void setup() {
   // put your setup code here, to run once:
  pinMode(solenoidPin, OUTPUT);
 }
 
-void loop() {
+void loop() { 
   // put your main code here, to run repeatedly:
+  for(int i = 0 ; i < 1 ; i +=1){
+    halfNote();
+  }
 
+  for(int i = 0 ; i < 4 ; i ++){
+    quarterNote();
+  }
+  
+  for(int i = 0 ; i < 8 ; i ++){
+    eighthNote();
+  }
+
+ for(int i = 0 ; i < 16 ; i ++){
+    sixteenthNote();
+  }
+  
 
 }
 
@@ -38,4 +53,12 @@ void eighthNote(){
   delay(50);
   digitalWrite(solenoidPin, LOW);
   delay(bpm/60*1000/2-50);
+  }
+
+
+  void sixteenthNote(){
+  digitalWrite(solenoidPin,HIGH);
+  delay(50);
+  digitalWrite(solenoidPin, LOW);
+  delay(bpm/60*1000/4-50);
   }
